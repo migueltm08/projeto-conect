9dc64076ad688d2a78795b21eea17c87a07e7e9d
migueltm08 <migueltm08@gmail.com>
2025-08-16 15:36:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function togglemode() {
+   const html = document.documentElement
+
+   /*if(html.classList.contains('light')) {
+    html.classList.remove('light')
+    } else{
+        html.classList.add('light')
+    } */
+   html.classList.toggle('light')
+
+   //pegar a tag img
+   const img = document.querySelector("#profile img")
+    //substituir a img
+    if(html.classList.contains('light')) {
+        //se tiver lightmode, add a img
+        img.setAttribute('src',' ./assets/conect-branco.png')
+    } else{
+        //se n tiver lightmode, mantem
+        img.setAttribute('src','./assets/conect-preto.png')
+    }
+} 
